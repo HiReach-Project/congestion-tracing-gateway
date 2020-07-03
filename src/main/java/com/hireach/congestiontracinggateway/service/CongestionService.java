2b6f9982a9c4ee9facbff23755a9cec365652b4b
codr <codrut.panea@esolutions.ro>
2020-07-03 10:46:06 +0300

    small refactor

--- src/main/java/com/hireach/congestiontracinggateway/service/CongestionService.java
+++ src/main/java/com/hireach/congestiontracinggateway/service/CongestionService.java
@@ -1,18 +1,23 @@
 package com.hireach.congestiontracinggateway.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class CongestionService {
 
-    private final HashMap<String, String> nodeUrls = new HashMap<>() {{
-        put("http://localhost:8091/api/congestion", "HM4fQAQmLF36LCWEctCuJrEtqTtgegpG"); // HiReach
-        put("http://localhost:8092/api/congestion", "HhPafhF9dwA3gC77PsdwrDFtydkYsnCc"); // UPB
+    @Value("${api.gateway.key}")
+    private String apiGatewayKey;
+
+    private final List<String> nodeUrls = new ArrayList<>() {{
+        add("http://localhost:8091/hireach/api/congestion"); // HiReach
+        add("http://localhost:8092/upb/api/congestion"); // UPB
     }};
 
     private final WebClient webClient;
@@ -23,16 +28,15 @@ public class CongestionService {
 
     public int getCongestion(double lat, double lon, double radius, Integer secondsAgo) {
 
-        // Get all nodes -> foreach -> get congestion -> sum
         AtomicInteger congestion = new AtomicInteger(0);
 
-        nodeUrls.forEach((url, pass) -> {
+        nodeUrls.forEach((url) -> {
             Integer nodeCongestion = webClient
                     .get()
                     .uri(url + "?lat=" + lat
                             + "&lon=" + lon
                             + "&radius=" + radius
-                            + "&key=" + pass
+                            + "&key=" + apiGatewayKey
                     )
                     .retrieve()
                     .bodyToMono(new ParameterizedTypeReference<Integer>() {
