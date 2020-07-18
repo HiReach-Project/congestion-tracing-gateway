69aacf8c077387cb9b35929b93fd6ac6ccb13376
codr <codrut.panea@esolutions.ro>
2020-07-18 22:04:10 +0300

    refactor exception handling

--- src/main/java/com/hireach/congestiontracinggateway/CongestionTracingGatewayApplication.java
+++ src/main/java/com/hireach/congestiontracinggateway/CongestionTracingGatewayApplication.java
@@ -3,10 +3,11 @@ package com.hireach.congestiontracinggateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableCaching
+@EnableWebMvc
 public class CongestionTracingGatewayApplication {
 
     public static void main(String[] args) {
